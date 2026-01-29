df6792e737150f35e5442b923fff9881dee028cd
HasiniSachintha <hasinisachintha22@gmail.com>
2026-01-29 13:57:00 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,19 @@
 package main.java.com.example;
 
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    public static void main(String[] args) throws SQLException {
+        new App().run();
+    }
+
+    public void run() throws SQLException {
         Calculator calc = new Calculator();
-        System.out.println(calc.calculate(10, 5, "add-again"));
+        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
         UserService service = new UserService();
         service.findUser("admin");
         service.deleteUser("admin"); // NEW dangerous call
